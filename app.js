63f3f4faf63f6fb7170ea8afe15396073d86a5c8
Oladele Soyemi <horlerebook@gmail.com>
2020-02-15 15:48:09 +0100

    Intial Commit. Project Version 1.0

---
+++ app.js
@@ -0,0 +1,25 @@
+let modalTop = document.getElementById('modal-top');
+let modalRight = document.getElementById('modal-right');
+let modalLeft = document.getElementById('modal-left');
+let modalBottom = document.getElementById('modal-bottom');
+let modalCenter = document.getElementById('modal-center');
+
+modalTop.addEventListener('click', function (){
+    document.querySelector('#top-modal').style.display = 'flex';
+} );
+
+modalRight.addEventListener('click', function (){
+    document.querySelector('#right-modal').style.display = 'flex';
+} );
+
+modalLeft.addEventListener('click', function (){
+    document.querySelector('#left-modal').style.display = 'flex';
+} );
+
+modalBottom.addEventListener('click', function (){
+    document.querySelector('#bottom-modal').style.display = 'flex';
+} );
+
+modalCenter.addEventListener('click', function (){
+    document.querySelector('#center-modal').style.display = 'flex';
+} );
